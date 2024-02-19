144a687b757686ae692e193083b2933e37d4a32d
dsriggs1 <dsriggs1@gmail.com>
2024-02-18 20:06:27 -0500

    Adding flake.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+# /etc/nixos/flake.nix
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
