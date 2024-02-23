2f78fedb36d29db273d472bc7bd10ed0b7dd6415
dsriggs1 <dsriggs1@gmail.com>
2024-02-23 14:04:55 -0500

    Adding flake files, not sure if this is working yet, but want to track these in case I mess something up.

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
-# /etc/nixos/flake.nix
 {
-  description = "My NixOS configuration";
+  description = "NixOS configuration for my personal laptop";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    # Please replace my-nixos with your hostname
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sean = import ./home.nix;
+          }
         ];
       };
     };
