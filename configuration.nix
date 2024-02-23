ce1c9b1724758f92ff04edc96258bb01254174c2
dsriggs1 <dsriggs1@gmail.com>
2024-02-23 14:04:03 -0500

    Commenting out the home-manager configuration in the configuration.nix file, moved to standalone home.nix.

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
+      #<home-manager/nixos>
     ];
 
    nix = {
@@ -18,56 +18,56 @@
     '';
   };
 
-  home-manager.users.sean = { pkgs, ... }: {
-    nixpkgs.config = {
-      allowUnfree = true;
-    }; 
-    home.packages = [ pkgs.atool pkgs.httpie ];
-    programs.git = {
-      enable = true;
-      userName = "dsriggs1";
-      userEmail = "dsriggs1@gmail.com";
-      aliases =
-        {
-          a = "add";
-          c = "commit";
-          ca = "commit --amend";
-          can = "commit --amend --no-edit";
-          cl = "clone";
-          cm = "commit -m";
-          co = "checkout";
-          cp = "cherry-pick";
-          cpx = "cherry-pick -x";
-          d = "diff";
-          f = "fetch";
-          fo = "fetch origin";
-          fu = "fetch upstream";
-          gds = "diff --staged";
-          lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-          lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-          pl = "pull";
-          pr = "pull -r";
-          ps = "push";
-          psf = "push -f";
-          rb = "rebase";
-          rbi = "rebase -i";
-          r = "remote";
-          ra = "remote add";
-          rr = "remote rm";
-          rv = "remote -v";
-          rs = "remote show";
-          st = "status";
-      };
-    };
+  # home-manager.users.sean = { pkgs, ... }: {
+  #   nixpkgs.config = {
+  #     allowUnfree = true;
+  #   }; 
+  #   home.packages = [ pkgs.atool pkgs.httpie ];
+  #   programs.git = {
+  #     enable = true;
+  #     userName = "dsriggs1";
+  #     userEmail = "dsriggs1@gmail.com";
+  #     aliases =
+  #       {
+  #         a = "add";
+  #         c = "commit";
+  #         ca = "commit --amend";
+  #         can = "commit --amend --no-edit";
+  #         cl = "clone";
+  #         cm = "commit -m";
+  #         co = "checkout";
+  #         cp = "cherry-pick";
+  #         cpx = "cherry-pick -x";
+  #         d = "diff";
+  #         f = "fetch";
+  #         fo = "fetch origin";
+  #         fu = "fetch upstream";
+  #         gds = "diff --staged";
+  #         lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+  #         lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+  #         pl = "pull";
+  #         pr = "pull -r";
+  #         ps = "push";
+  #         psf = "push -f";
+  #         rb = "rebase";
+  #         rbi = "rebase -i";
+  #         r = "remote";
+  #         ra = "remote add";
+  #         rr = "remote rm";
+  #         rv = "remote -v";
+  #         rs = "remote show";
+  #         st = "status";
+  #     };
+  #   };
 
-    programs.vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        github.copilot
-      ];
-    };    
-    home.stateVersion = "23.11";
-  };
+  #   programs.vscode = {
+  #     enable = true;
+  #     extensions = with pkgs.vscode-extensions; [
+  #       github.copilot
+  #     ];
+  #   };    
+  #   home.stateVersion = "23.11";
+  # };
 
 
   # Bootloader.
