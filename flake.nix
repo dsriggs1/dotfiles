1c2509dee10b80dd31c914350bcd2c08006073ff
dsriggs1 <dsriggs1@gmail.com>
2024-02-29 20:21:02 -0500

    Adding the unstable branch to flake.

--- flake.nix
+++ flake.nix
@@ -4,17 +4,30 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/01885a071465";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager, 
+    ... 
+  }: {
     # Please replace my-nixos with your hostname
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
+        specialArgs = {
+          pkgs-unstable = import nixpkgs-unstable {
+          system = system;
+          config.allowUnfree = true;
+        };
+      };  
         modules = [
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
