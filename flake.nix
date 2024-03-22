bf25acc7bcb5570434d171e856dfb8805cd617b5
dsriggs1 <dsriggs1@gmail.com>
2024-03-22 13:56:32 -0400

    Adding firefox-addons to flake.

--- flake.nix
+++ flake.nix
@@ -10,16 +10,27 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{
+  outputs = {
     self,
     nixpkgs,
     nixpkgs-unstable,
     nur,
     home-manager, 
+    firefox-addons,
     ... 
-  }: {
+  } @inputs: 
+  let 
+    inherit (self) outputs;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { system = "x86_64-linux"; config = { allowUnfree = true;  }; };    #addon-pkgs = pkgs.callPackage firefox-addons { };
+
+  in {
     # Please replace my-nixos with your hostname
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem rec {
@@ -28,11 +39,18 @@
           pkgs-unstable = import nixpkgs-unstable {
           system = system;
           config.allowUnfree = true;
+         #inherit inputs outputs firefox-addons;
+          
         };
-        nur = import nur {
-            system = system;
-            config.allowUnfree = true;
+          nur = import nur {
+              system = system;
+              config.allowUnfree = true;
           };
+          firefox-addons = import firefox-addons {
+              system = system;
+              config.allowUnfree = true;
+          };
+          
       };  
         modules = [
           # Import the previous configuration.nix we used,
@@ -42,10 +60,27 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.sean = import ./home.nix;
+            home-manager.users.sean = import ./home.nix {
+              inherit pkgs inputs ;
+              config = pkgs.config;
+             };
           }
         ];
       };
     };
+    # homeConfigurations = {
+    #   "sean@nixos" = home-manager.lib.homeManagerConfiguration {
+    #     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    #     extraSpecialArgs = {inherit inputs outputs;};
+    #     modules = [
+    #       ./home.nix
+    #     ];
+    #   };
+    # };
+    # homeConfigurations."sean@nixos" = home-manager.lib.homeManagerConfiguration {
+    #   inherit pkgs;
+    #   extraSpecialArgs = { inherit inputs; };
+    #   modules = [ ./home.nix ];
+    # };
   };
 }
