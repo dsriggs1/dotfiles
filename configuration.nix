d26d07424ab45a45e303af0de564e0cfbbcd78cc
dsriggs1 <dsriggs1@gmail.com>
2024-04-03 17:49:00 -0400

    Removing home-manager comments no longer needed in configuration.nix file.

--- configuration.nix
+++ configuration.nix
@@ -21,57 +21,6 @@
   #Auto update nix-store at each build
   nix.settings.auto-optimise-store = true;
 
-  # home-manager.users.sean = { pkgs, ... }: {
-  #   nixpkgs.config = {
-  #     allowUnfree = true;
-  #   }; 
-  #   home.packages = [ pkgs.atool pkgs.httpie ];
-  #   programs.git = {
-  #     enable = true;
-  #     userName = "dsriggs1";
-  #     userEmail = "dsriggs1@gmail.com";
-  #     aliases =
-  #       {
-  #         a = "add";
-  #         c = "commit";
-  #         ca = "commit --amend";
-  #         can = "commit --amend --no-edit";
-  #         cl = "clone";
-  #         cm = "commit -m";
-  #         co = "checkout";
-  #         cp = "cherry-pick";
-  #         cpx = "cherry-pick -x";
-  #         d = "diff";
-  #         f = "fetch";
-  #         fo = "fetch origin";
-  #         fu = "fetch upstream";
-  #         gds = "diff --staged";
-  #         lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-  #         lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-  #         pl = "pull";
-  #         pr = "pull -r";
-  #         ps = "push";
-  #         psf = "push -f";
-  #         rb = "rebase";
-  #         rbi = "rebase -i";
-  #         r = "remote";
-  #         ra = "remote add";
-  #         rr = "remote rm";
-  #         rv = "remote -v";
-  #         rs = "remote show";
-  #         st = "status";
-  #     };
-  #   };
-
-  #   programs.vscode = {
-  #     enable = true;
-  #     extensions = with pkgs.vscode-extensions; [
-  #       github.copilot
-  #     ];
-  #   };    
-  #   home.stateVersion = "23.11";
-  # };
-
 
   # Bootloader.
   boot.loader.grub.enable = true;
