40d7fd7ceb8bbd05479342a9fb3ea57d34828a92
dsriggs1 <dsriggs1@gmail.com>
2024-04-04 13:27:39 -0400

    Adding nixarr module file, this is just the default at the moment. Seems to mostly work, but will probably want to reconfigure this at some point.

--- nixarr.nix
+++ nixarr.nix
@@ -1,41 +1,44 @@
- nixarr = {
-    enable = true;
-    # These two values are also the default, but you can set them to whatever
-    # else you want
-    # WARNING: Do _not_ set them to `/home/user/whatever`, it will not work!
-    mediaDir = "/data/media";
-    stateDir = "/data/media/.state/nixarr";
-
-    vpn = {
+ {config, pkgs, nixarr,...}:
+ {
+  nixarr = {
       enable = true;
-      # WARNING: This file must _not_ be in the config git directory
-      # You can usually get this wireguard file from your VPN provider
-      wgConf = "/data/.secret/wg.conf";
-    };
+      # These two values are also the default, but you can set them to whatever
+      # else you want
+      # WARNING: Do _not_ set them to `/home/user/whatever`, it will not work!
+      mediaDir = "/data/media";
+      stateDir = "/data/media/.state/nixarr";
 
-    jellyfin = {
-      enable = true;
-      # These options set up a nginx HTTPS reverse proxy, so you can access
-      # Jellyfin on your domain with HTTPS
-      expose.https = {
+      vpn = {
         enable = true;
-        domainName = "your.domain.com";
-        acmeMail = "your@email.com"; # Required for ACME-bot
+        # WARNING: This file must _not_ be in the config git directory
+        # You can usually get this wireguard file from your VPN provider
+        wgConf = "/data/.secret/wg.conf";
       };
-    };
 
-    transmission = {
-      enable = true;
-      vpn.enable = true;
-      peerPort = 50000; # Set this to the port forwarded by your VPN
-    };
+      jellyfin = {
+        enable = true;
+        # These options set up a nginx HTTPS reverse proxy, so you can access
+        # Jellyfin on your domain with HTTPS
+        expose.https = {
+          enable = true;
+          domainName = "your.domain.com";
+          acmeMail = "your@email.com"; # Required for ACME-bot
+        };
+      };
 
-    # It is possible for this module to run the *Arrs through a VPN, but it
-    # is generally not recommended, as it can cause rate-limiting issues.
-    bazarr.enable = true;
-    lidarr.enable = true;
-    prowlarr.enable = true;
-    radarr.enable = true;
-    readarr.enable = true;
-    sonarr.enable = true;
-  };
+      transmission = {
+        enable = true;
+        vpn.enable = true;
+        peerPort = 50000; # Set this to the port forwarded by your VPN
+      };
+
+      # It is possible for this module to run the *Arrs through a VPN, but it
+      # is generally not recommended, as it can cause rate-limiting issues.
+      bazarr.enable = true;
+      lidarr.enable = true;
+      prowlarr.enable = true;
+      radarr.enable = true;
+      readarr.enable = true;
+      sonarr.enable = true;
+    };
+ }
