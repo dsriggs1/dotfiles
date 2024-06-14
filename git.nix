78647f91325b1566de194f6a89a1b90575e82671
dsriggs1 <dsriggs1@gmail.com>
2024-06-14 16:20:21 -0400

    Adding modular git configuration.

---
+++ git.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ...}:
+{
+  programs.git = {
+      enable = true;
+      userName = "dsriggs1";
+      userEmail = "dsriggs1@gmail.com";
+      aliases =
+        {
+          a = "add";
+          c = "commit";
+          ca = "commit --amend";
+          can = "commit --amend --no-edit";
+          cl = "clone";
+          cm = "commit -m";
+          co = "checkout";
+          cp = "cherry-pick";
+          cpx = "cherry-pick -x";
+          d = "diff";
+          f = "fetch";
+          fo = "fetch origin";
+          fu = "fetch upstream";
+          gds = "diff --staged";
+          lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+          lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+          pl = "pull";
+          pr = "pull -r";
+          ps = "push";
+          psf = "push -f";
+          rb = "rebase";
+          rbi = "rebase -i";
+          r = "remote";
+          ra = "remote add";
+          rr = "remote rm";
+          rv = "remote -v";
+          rs = "remote show";
+          st = "status";
+      };
+    };
+}
