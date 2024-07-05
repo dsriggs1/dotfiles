8a9b66cb8b24cf133e015c5f4f272fdf7ecc7640
dsriggs1 <dsriggs1@gmail.com>
2024-07-05 18:25:24 -0400

    Adjusting the layout of the conform module.

--- conform.nix
+++ conform.nix
@@ -1,5 +1,25 @@
 {
-  programs.nixvim = {
+
+ programs.nixvim = {
+  plugins.conform-nvim = {
+    enable = true;
+    notifyOnError = true;
+    formattersByFt = {
+      html = [["prettierd" "prettier"]];
+      css = [["prettierd" "prettier"]];
+      javascript = [["prettierd" "prettier"]];
+      javascriptreact = [["prettierd" "prettier"]];
+      typescript = [["prettierd" "prettier"]];
+      typescriptreact = [["prettierd" "prettier"]];
+      java = ["google-java-format"];
+      python = ["black"];
+      lua = ["stylua"];
+      nix = ["alejandra"];
+      markdown = [["prettierd" "prettier"]];
+      rust = ["rustfmt"];
+    };
+  };
+
   keymaps = [
     {
       mode = "n";
@@ -20,7 +40,7 @@
       };
     }
 
-    {
+{
       mode = "v";
       key = "<leader>cF";
       action = "<cmd>lua require('conform').format()<cr>";
@@ -31,76 +51,46 @@
     }
   ];
 
-  extraConfigLuaPre = ''
-        local slow_format_filetypes = {}
+  extraConfigLua = ''
+    local conform = require("conform")
+    local notify = require("notify")
 
-        vim.api.nvim_create_user_command("FormatDisable", function(args)
-           if args.bang then
-            -- FormatDisable! will disable formatting just for this buffer
-            vim.b.disable_autoformat = true
-          else
-            vim.g.disable_autoformat = true
-          end
-        end, {
-          desc = "Disable autoformat-on-save",
-          bang = true,
-        })
-        vim.api.nvim_create_user_command("FormatEnable", function()
-          vim.b.disable_autoformat = false
-          vim.g.disable_autoformat = false
-        end, {
-          desc = "Re-enable autoformat-on-save",
-        })
-        vim.api.nvim_create_user_command("FormatToggle", function(args)
-          if args.bang then
-            -- Toggle formatting for current buffer
-            vim.b.disable_autoformat = not vim.b.disable_autoformat
-          else
-            -- Toggle formatting globally
-            vim.g.disable_autoformat = not vim.g.disable_autoformat
-          end
-        end, {
-          desc = "Toggle autoformat-on-save",
-          bang = true,
-        })
-      '';
+    conform.setup({
+      format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
+    })
+
+    local function show_notification(message, level)
+      notify(message, level, { title = "conform.nvim" })
+    end
+
+    vim.api.nvim_create_user_command("FormatToggle", function(args)
+      local is_global = not args.bang
+      if is_global then
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      if vim.g.disable_autoformat then
+        show_notification("Autoformat-on-save disabled globally", "info")
+      else
+        show_notification("Autoformat-on-save enabled globally", "info")
+      end
+      else
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+      if vim.b.disable_autoformat then
+        show_notification("Autoformat-on-save disabled for this buffer", "info")
+      else
+        show_notification("Autoformat-on-save enabled for this buffer", "info")
+        end
+      end
+    end, {
+      desc = "Toggle autoformat-on-save",
+      bang = true,
+    })
+  '';
   };
-
-  programs.nixvim = {
-  plugins.conform-nvim = {
-    enable = true;
-    notifyOnError = true;
-
-    formatOnSave = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-
-          if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              slow_format_filetypes[vim.bo[bufnr].filetype] = true
-            end
-          end
-
-          return { timeout_ms = 200, lsp_fallback = true }, on_format
-         end
-      '';
-
-    formattersByFt = {
-      java = ["google-java-format"];
-      python = ["black"];
-      lua = ["stylua"];
-      nix = ["alejandra"];
-      rust = ["rustfmt"];
-    };
-  }; 
-  };
-
 }
-
 
