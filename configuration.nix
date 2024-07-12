b6bb13b6b22e755a383e2f9a23b3efc66df28bc0
dsriggs1 <dsriggs1@gmail.com>
2024-07-12 17:56:47 -0400

    Commenting out unused references in configuration file.

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  config,
+  #  config,
   pkgs,
   pkgs-unstable,
-  nixarr,
-  inputs,
+  # nixarr,
+  #inputs,
   ...
 }: {
   imports = [
