152b65fea171631baedf5ab0d690435d29adfb1d
dsriggs1 <dsriggs1@gmail.com>
2024-07-14 20:00:08 -0400

    Refactoring and formatting of flake file.

--- flake.nix
+++ flake.nix
@@ -3,17 +3,15 @@
 
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
-    #nixpkgs.url = "github:NixOS/nixpkgs/01885a071465";
-    # nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
-    stylix.url = "github:danth/stylix/release-23.11";  
+    stylix.url = "github:danth/stylix/release-23.11";
     nixvim = {
-    	url = "github:nix-community/nixvim/nixos-24.05";
-	    inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-      home-manager = {
+    home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -21,11 +19,10 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixarr= {
+    nixarr = {
       url = "github:rasmus-kirk/nixarr";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = {
@@ -33,18 +30,18 @@
     nixpkgs,
     nixpkgs-unstable,
     nur,
-    home-manager, 
+    home-manager,
     firefox-addons,
     nixarr,
     stylix,
     nixvim,
-    ... 
-  } @inputs: 
-  let 
-    inherit (self) outputs;
+    ...
+  } @ inputs: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { system = "x86_64-linux"; config = { allowUnfree = true;  }; };    #addon-pkgs = pkgs.callPackage firefox-addons { };
-
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config = {allowUnfree = true;};
+    }; #addon-pkgs = pkgs.callPackage firefox-addons { };
   in {
     # Please replace my-nixos with your hostname
     nixosConfigurations = {
@@ -52,41 +49,40 @@
         system = "x86_64-linux";
         specialArgs = {
           pkgs-unstable = import nixpkgs-unstable {
-          system = system;
-          config.allowUnfree = true;
-          
-        };
+            system = system;
+            config.allowUnfree = true;
+          };
           nur = import nur {
-              system = system;
-              config.allowUnfree = true;
+            system = system;
+            config.allowUnfree = true;
           };
           firefox-addons = import firefox-addons {
-              system = system;
-              config.allowUnfree = true;
+            system = system;
+            config.allowUnfree = true;
           };
           nixarr = import nixarr {
-              system = system;
-              config.allowUnfree = true;
+            system = system;
+            config.allowUnfree = true;
           };
           stylix = import stylix {
-              system = system;
-              config.allowUnfree = true;
+            system = system;
+            config.allowUnfree = true;
           };
           nixvim = import nixvim {
-              system = system;
-              config.allowUnfree = true;
+            system = system;
+            config.allowUnfree = true;
           };
 
-          inherit pkgs inputs ;      
-      };  
+          inherit pkgs inputs;
+        };
         modules = [
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
-            ./configuration.nix
+          ./configuration.nix
           #./nixos/servarr/configuration.nix
-            nixarr.nixosModules.default
-            stylix.nixosModules.stylix
-            home-manager.nixosModules.home-manager
+          nixarr.nixosModules.default
+          stylix.nixosModules.stylix
+          home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -94,16 +90,15 @@
               nixvim.homeManagerModules.nixvim
             ];
             home-manager.users.sean = import ./home.nix {
-              
               inherit pkgs inputs;
               config = pkgs.config;
-              stylix.targets.xyz.enable = false;              
-             };
+              stylix.targets.xyz.enable = false;
+            };
           }
         ];
       };
     };
-      
+
     # homeConfigurations = {
     #   "sean@nixos" = home-manager.lib.homeManagerConfiguration {
     #     pkgs = nixpkgs.legacyPackages.x86_64-linux;
