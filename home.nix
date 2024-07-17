5469839b0438360a077e8a9c39aad1b9ada8d30e
dsriggs1 <dsriggs1@gmail.com>
2024-07-17 14:23:27 -0400

    Moving the git.nix file to sub directory.

--- home.nix
+++ home.nix
@@ -1,39 +1,42 @@
-{ config, pkgs, inputs,  ... }:
-
 {
-    imports = [
-      ./sh.nix
-      # ./firefox.nix
-      ./git.nix
-      ./vscode.nix
-      ./nixvim.nix
-    ];
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./sh.nix
+    # ./firefox.nix
+    ./user/app/git/git.nix
+    ./vscode.nix
+    ./neovim/nixvim.nix
+  ];
 
-    home.username = "sean";
-    home.homeDirectory = "/home/sean";
+  home.username = "sean";
+  home.homeDirectory = "/home/sean";
 
   #  stylix.image = /home/sean/wallpapers/lake-sunrise.jpg;
-    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
 
-    home.packages = with pkgs; [
-        git
-        vscode
-        firefox   
-    ];       
-    
+  home.packages = with pkgs; [
+    git
+    vscode
+    firefox
+  ];
+
   #   programs.nixvim = {
   #     options.completeopt = ["menu" "menuone" "noselect"];
-  #    	enable = true; 
-      
+  #    	enable = true;
+
   #     globals.mapleader = " ";
 
-       #options = {
-         #number = true;         # Show line numbers
+  #options = {
+  #number = true;         # Show line numbers
   #  #   	relativenumber = true; # Show relative line numbers
 
   #     	shiftwidth = 2;        # Tab width should be 2
-     # };
-     
+  # };
+
   #      keymaps = [
   #     {
   #       action = "<cmd>Telescope live_grep<CR>";
@@ -45,7 +48,6 @@
   #       options.desc = "Toggle NvimTree";
   #     }
   #    ];
-       
 
   #     plugins = {
   #       nvim-tree = {
@@ -53,10 +55,10 @@
   #         openOnSetupFile = true;
   #         autoReloadOnWrite = true;
   #       };
-        
+
   #       codeium-nvim.enable = true;
   #       treesitter.enable = true;
-        
+
   #       cmp = {
   #           enable = true;
   #           # menu = {
@@ -72,10 +74,10 @@
 
   #       lsp = {
   #         enable = true;
-          
+
   #         servers = {
   #           tsserver.enable = true;
-      
+
   #           nil-ls ={
   #             enable = true;
   #           };
@@ -93,31 +95,31 @@
   #           pyright = {
   #             enable = true;
   #           };
-  #         };  
+  #         };
   #       };
   #   };
-   
-    # plugins.nvim-tree = {
-    # 	enable = true;
-    # };
-    
-    # plugins.cmp = {
-    #     enable = true;
-    #     autoEnableSources = true;
-    #       sources = [
-    #         {name = "nvim_lsp";}
-    #     #   {name = "path";}
-    #     #   {name = "buffer";}
-    #     #   {name = "luasnip";}
-    #      ];
-    #   };    
+
+  # plugins.nvim-tree = {
+  # 	enable = true;
+  # };
+
+  # plugins.cmp = {
+  #     enable = true;
+  #     autoEnableSources = true;
+  #       sources = [
+  #         {name = "nvim_lsp";}
+  #     #   {name = "path";}
+  #     #   {name = "buffer";}
+  #     #   {name = "luasnip";}
+  #      ];
+  #   };
 
   #  plugins.lsp = {
   #     enable = true;
-      
+
   #     servers = {
   #       tsserver.enable = true;
-	
+
   #       nil-ls ={
   #         enable = true;
   #       };
@@ -135,104 +137,102 @@
   #       pyright = {
   #         enable = true;
   #       };
-  #     };  
+  #     };
   #   };
 
-    # plugins.treesitter = { 
-    #   enable = true;
-    # };
+  # plugins.treesitter = {
+  #   enable = true;
+  # };
 
-    # plugins.codeium-nvim = {
-    #   enable = true;
-    # };
-  # };	
+  # plugins.codeium-nvim = {
+  #   enable = true;
+  # };
+  # };
 
-    # programs.vscode = {
-    #   enable = true;
-    #   extensions = with pkgs.vscode-extensions; [
-    #     github.copilot
-    #     ms-python.python
-    #     jnoortheen.nix-ide        
-    #   ];
-      
-    # };
- 
-    programs.firefox = {
-      enable = true;
-      profiles = {
-        default = {
-          extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+  # programs.vscode = {
+  #   enable = true;
+  #   extensions = with pkgs.vscode-extensions; [
+  #     github.copilot
+  #     ms-python.python
+  #     jnoortheen.nix-ide
+  #   ];
+
+  # };
+
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
           ublock-origin
-          ];
-          bookmarks = {
-            toolbar = {
+        ];
+        bookmarks = {
+          toolbar = {
             name = "My Toolbar";
             toolbar = true;
-                      
-          bookmarks = [
-            {
-              name = "Nixos Wiki";
-              tags = [ "nixos" ];
-              keyword = "nixos";
-              url = "https://wiki.nixos.org/wiki/NixOS_Wiki"; 
-            }
-            {
-              
-              name = "Jellyfin Server";
-              tags = [ "Jellyfin" ];
-              keyword = "Jellyfin";
-              url = "http://localhost:8096";              
-            }
 
-            {
-              name = "Bazarr";
-              tags = [ "Bazarr" ];
-              keyword = "Bazarr";
-              url = "http://localhost:6767";
-            }
+            bookmarks = [
+              {
+                name = "Nixos Wiki";
+                tags = ["nixos"];
+                keyword = "nixos";
+                url = "https://wiki.nixos.org/wiki/NixOS_Wiki";
+              }
+              {
+                name = "Jellyfin Server";
+                tags = ["Jellyfin"];
+                keyword = "Jellyfin";
+                url = "http://localhost:8096";
+              }
 
-            {  
-              name = "Lidarr";
-              tags = [ "Lidarr" ];
-              keyword = "Lidarr";
-              url = "http://localhost:8686";
-            }
+              {
+                name = "Bazarr";
+                tags = ["Bazarr"];
+                keyword = "Bazarr";
+                url = "http://localhost:6767";
+              }
 
-            {
-              name = "Prowlarr";
-              tags = [ "Prowlarr" ];
-              keyword = "Prowlarr";
-              url = "http://localhost:9696";
-            }
+              {
+                name = "Lidarr";
+                tags = ["Lidarr"];
+                keyword = "Lidarr";
+                url = "http://localhost:8686";
+              }
 
-            {
-              name = "Radarr";
-              tags = [ "Radarr" ];
-              keyword = "Radarr";
-              url = "http://localhost:7878";
-            }
-            
-            {
-              name = "Sonarr";
-              tags = [ "Sonarr" ];
-              keyword = "Sonarr";
-              url = "http://localhost:8989";
-            }
+              {
+                name = "Prowlarr";
+                tags = ["Prowlarr"];
+                keyword = "Prowlarr";
+                url = "http://localhost:9696";
+              }
 
-            {
-              name = "github";
-              tags = [ "github" ];
-              keyword = "github";
-              url = "https://github.com/";
-              
-            }            
-          ]; 
+              {
+                name = "Radarr";
+                tags = ["Radarr"];
+                keyword = "Radarr";
+                url = "http://localhost:7878";
+              }
+
+              {
+                name = "Sonarr";
+                tags = ["Sonarr"];
+                keyword = "Sonarr";
+                url = "http://localhost:8989";
+              }
+
+              {
+                name = "github";
+                tags = ["github"];
+                keyword = "github";
+                url = "https://github.com/";
+              }
+            ];
           };
-          };           
-        };         
+        };
       };
     };
-       
-    home.stateVersion = "23.11";
-    programs.home-manager.enable = true;
+  };
+
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
 }
