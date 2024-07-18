6cdfecb06d594130688e51d468856316f1d8d34d
dsriggs1 <dsriggs1@gmail.com>
2024-07-18 13:55:25 -0400

    Adding startship.nix terminal prompt module.

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
     # ./firefox.nix
     ./user/app/git/git.nix
     ./user/app/vscode/vscode.nix
+    ./user/app/browser/firefox.nix
     ./neovim/nixvim.nix
   ];
 
@@ -22,80 +23,6 @@
     vscode
     firefox
   ];
-
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      default = {
-        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-          ublock-origin
-        ];
-        bookmarks = {
-          toolbar = {
-            name = "My Toolbar";
-            toolbar = true;
-
-            bookmarks = [
-              {
-                name = "Nixos Wiki";
-                tags = ["nixos"];
-                keyword = "nixos";
-                url = "https://wiki.nixos.org/wiki/NixOS_Wiki";
-              }
-              {
-                name = "Jellyfin Server";
-                tags = ["Jellyfin"];
-                keyword = "Jellyfin";
-                url = "http://localhost:8096";
-              }
-
-              {
-                name = "Bazarr";
-                tags = ["Bazarr"];
-                keyword = "Bazarr";
-                url = "http://localhost:6767";
-              }
-
-              {
-                name = "Lidarr";
-                tags = ["Lidarr"];
-                keyword = "Lidarr";
-                url = "http://localhost:8686";
-              }
-
-              {
-                name = "Prowlarr";
-                tags = ["Prowlarr"];
-                keyword = "Prowlarr";
-                url = "http://localhost:9696";
-              }
-
-              {
-                name = "Radarr";
-                tags = ["Radarr"];
-                keyword = "Radarr";
-                url = "http://localhost:7878";
-              }
-
-              {
-                name = "Sonarr";
-                tags = ["Sonarr"];
-                keyword = "Sonarr";
-                url = "http://localhost:8989";
-              }
-
-              {
-                name = "github";
-                tags = ["github"];
-                keyword = "github";
-                url = "https://github.com/";
-              }
-            ];
-          };
-        };
-      };
-    };
-  };
 
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
