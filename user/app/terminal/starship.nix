5299a9087a2429a94c35a2c5e64ba32d7ca75037
dsriggs1 <dsriggs1@gmail.com>
2024-07-19 17:53:44 -0400

    Adding some basic starship configuration, still don't really have a handle on this.

--- user/app/terminal/starship.nix
+++ user/app/terminal/starship.nix
@@ -1,5 +1,16 @@
 {...}: {
   programs.starship = {
-    enabled = true;
+    enable = true;
   };
+  programs.starship.settings = {
+    # See docs here: https://starship.rs/config/
+    # Symbols config configured ./starship-symbols.nix.
+    hostname.style = "bold pink";
+    #hostname.ssh_only = false;
+    username.show_always = true;
+    username.style_user = "bold red"; # don't like the default
+    "$schema" = "https://starship.rs/config-schema.json";
+  };
+  programs.dircolors.enableBashIntegration = true;
+  programs.starship.enableBashIntegration = true;
 }
