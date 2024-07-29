5599f9ab99881ba740ecff37456fa96b4a1a7067
dsriggs1 <dsriggs1@gmail.com>
2024-07-29 14:41:59 -0400

    Adding some terminal modules to home-manager.

--- home.nix
+++ home.nix
@@ -1,27 +1,32 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
-    ./sh.nix
+    ./user/app/terminal/sh.nix
     # ./firefox.nix
     ./user/app/git/git.nix
     ./user/app/vscode/vscode.nix
     ./user/app/browser/firefox.nix
     ./neovim/nixvim.nix
+    ./user/app/tmux/tmux.nix
+    ./user/app/tmux/tmuxinator.nix
+    ./user/app/terminal/starship.nix
   ];
 
   home.username = "sean";
   home.homeDirectory = "/home/sean";
 
+  # Example themes: https://github.com/tinted-theming/schemes/tree/spec-0.11/base16
+  # Some example color schemes that can be turned on/off
   #  stylix.image = /home/sean/wallpapers/lake-sunrise.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-
+  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
+  #stylix.base16Scheme = "/home/sean/Downloads/dotfiles/themes/cyborg_girl";
   home.packages = with pkgs; [
     git
     vscode
     firefox
+    tmux
+    starship
+    tmuxinator
   ];
 
   home.stateVersion = "23.11";
