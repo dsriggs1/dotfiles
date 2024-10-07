417754433996f04684aa26b6108cfacc1d6500b2
dsriggs1 <dsriggs1@gmail.com>
2024-10-07 02:22:11 -0400

    Minor refactoring

--- flake.nix
+++ flake.nix
@@ -80,82 +80,82 @@
     }; #addon-pkgs = pkgs.callPackage firefox-addons { };
   in {
     # Please replace my-nixos with your hostname
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit systemSettings;
-          pkgs-unstable = import nixpkgs-unstable {
-            system = system;
-            config.allowUnfree = true;
-          };
-
-          nur = import nur {
-            system = system;
-            config.allowUnfree = true;
-          };
-          firefox-addons = import firefox-addons {
-            system = system;
-            config.allowUnfree = true;
-          };
-          nixarr = import nixarr {
-            system = system;
-            config.allowUnfree = true;
-          };
-          stylix = import stylix {
-            system = system;
-            config.allowUnfree = true;
-          };
-          nixvim = import nixvim {
-            system = system;
-            config.allowUnfree = true;
-          };
-
-          inherit pkgs inputs;
+    nixosConfigurations.${systemSettings.hostname} = nixpkgs.lib.nixosSystem rec {
+      #inherit systemSettings.system;
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit systemSettings;
+        pkgs-unstable = import nixpkgs-unstable {
+          system = system;
+          config.allowUnfree = true;
         };
-        modules = [
-          # Import the previous configuration.nix we used,
-          # so the old configuration file still takes effect
-          ./configuration.nix
-          #./nixos/servarr/configuration.nix
-          inputs.disko.nixosModules.disko
-          nixarr.nixosModules.default
-          stylix.nixosModules.stylix
-          inputs.impermanence.nixosModules.impermanence
-          home-manager.nixosModules.home-manager
-          (import ./disko-config.nix {device = "/dev/vda";})
-          {
-            #   home-manager.extraSpecialArgs = specialArgs;
 
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              inherit userSettings;
-              pkgs-unstable = import nixpkgs-unstable {
-                system = system;
-                config.allowUnfree = true;
-              };
-            };
+        nur = import nur {
+          system = system;
+          config.allowUnfree = true;
+        };
+        firefox-addons = import firefox-addons {
+          system = system;
+          config.allowUnfree = true;
+        };
+        nixarr = import nixarr {
+          system = system;
+          config.allowUnfree = true;
+        };
+        stylix = import stylix {
+          system = system;
+          config.allowUnfree = true;
+        };
+        nixvim = import nixvim {
+          system = system;
+          config.allowUnfree = true;
+        };
 
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.sharedModules = [
-              nixvim.homeManagerModules.nixvim
-              # inputs.impermanence.homeManagerModules.impermanence
-            ];
-            home-manager.users.sean = import ./home.nix {
-              inherit pkgs;
-              inherit userSettings;
-              pkgs-unstable = import nixpkgs-unstable {
-                system = system;
-                config.allowUnfree = true;
-              };
-
-              config = pkgs.config;
-              stylix.targets.xyz.enable = false;
-            };
-          }
-        ];
+        inherit pkgs inputs;
       };
+      modules = [
+        ./disko-config.nix
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        #./nixos/servarr/configuration.nix
+        inputs.disko.nixosModules.disko
+        nixarr.nixosModules.default
+        stylix.nixosModules.stylix
+        inputs.impermanence.nixosModules.impermanence
+        home-manager.nixosModules.home-manager
+        #   (import ./disko-config.nix {device = "/dev/vda";})
+        {
+          #   home-manager.extraSpecialArgs = specialArgs;
+
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+            inherit userSettings;
+            pkgs-unstable = import nixpkgs-unstable {
+              system = system;
+              config.allowUnfree = true;
+            };
+          };
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.sharedModules = [
+            nixvim.homeManagerModules.nixvim
+            # inputs.impermanence.homeManagerModules.impermanence
+          ];
+          home-manager.users.sean = import ./home.nix {
+            inherit pkgs;
+            inherit userSettings;
+            pkgs-unstable = import nixpkgs-unstable {
+              system = system;
+              config.allowUnfree = true;
+            };
+
+            config = pkgs.config;
+            stylix.targets.xyz.enable = false;
+          };
+        }
+      ];
     };
 
     # homeConfigurations = {
