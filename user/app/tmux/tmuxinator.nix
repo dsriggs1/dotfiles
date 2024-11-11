7456efc1b0e42cdeb214815a7be480c548bd09ae
dsriggs1 <dsriggs1@gmail.com>
2024-11-11 02:42:28 -0500

    Adding some additional tmuxinator configurations.

--- user/app/tmux/tmuxinator.nix
+++ user/app/tmux/tmuxinator.nix
@@ -7,9 +7,35 @@
         - main:
             layout: main-horizontal
             panes:
-              - vim
+              - nvim
               - shell: bash
 
+    '';
+  };
+  home.file = {
+    ".config/tmuxinator/nixos_nvim.yml".text = ''
+      name: nixos_nvim
+      root: ~/Downloads/dotfiles/neovim
+      windows:
+        - main:
+            layout: main-horizontal
+            panes:
+              - nvim
+              - shell: nushell
+
+    '';
+  };
+  home.file = {
+    ".config/tmuxinator/baseball.yml".text = ''
+      name: baseball
+      root: ~/Downloads/Baseball_Project/
+      windows:
+        - main:
+            layout: main-vertical
+            panes:
+              - nvim
+              - shell: nushell
+              - shell: mycli
     '';
   };
 }
