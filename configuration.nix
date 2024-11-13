acfc5cf11490778124aa469ab95a94e46c886dc8
dsriggs1 <dsriggs1@gmail.com>
2024-11-13 02:04:09 -0500

    Adding impermanence and persistence configuration.

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
     ./system/app/picom.nix
     ./system/wm/x11.nix
     ./system/security/sshd.nix
-    #    ./disko-config.nix
+    ./disko/btrfs-subvolumes.nix
     ./system/style/stylix.nix
   ];
 
@@ -97,45 +97,45 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
 
-  #  boot.initrd.postDeviceCommands = lib.mkAfter ''
-  #   mkdir /btrfs_tmp
-  #  mount /dev/root_vg/root /btrfs_tmp
-  # if [[ -e /btrfs_tmp/root ]]; then
-  #    mkdir -p /btrfs_tmp/old_roots
-  #   timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #  mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #fi
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+       mkdir /btrfs_tmp
+      mount /dev/root_vg/root /btrfs_tmp
+     if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+      mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
 
-  #delete_subvolume_recursively() {
-  #   IFS=$'\n'
-  #  for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #     delete_subvolume_recursively "/btrfs_tmp/$i"
-  # done
-  # btrfs subvolume delete "$1"
-  #}
+    delete_subvolume_recursively() {
+       IFS=$'\n'
+      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+         delete_subvolume_recursively "/btrfs_tmp/$i"
+     done
+     btrfs subvolume delete "$1"
+    }
 
-  #for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-  #   delete_subvolume_recursively "$i"
-  #done
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+       delete_subvolume_recursively "$i"
+    done
 
-  #btrfs subvolume create /btrfs_tmp/root
-  # umount /btrfs_tmp
-  #'';
+    btrfs subvolume create /btrfs_tmp/root
+     umount /btrfs_tmp
+  '';
 
-  #  fileSystems."/persist".neededForBoot = true;
-  # environment.persistence."/persist/system" = {
-  #  hideMounts = true;
-  # directories = [
-  #  "/etc/nixos"
-  # "/var/log"
-  # "/var/lib/bluetooth"
-  # "/var/lib/nixos"
-  # "/var/lib/systemd/coredump"
-  # "/etc/NetworkManager/system-connections"
-  # "/home/sean/Downloads"
-  # "/home/sean/Downloads/dotfiles"
-  # ];
-  # };
+  fileSystems."/persist".neededForBoot = true;
+  environment.persistence."/persist/system" = {
+    hideMounts = true;
+    directories = [
+      "/etc/nixos"
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      "/home/sean/Downloads"
+      "/home/sean/Downloads/dotfiles"
+    ];
+  };
 
   users.users.sean = {
     isNormalUser = true;
