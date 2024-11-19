01f84e7be9a39f30b914d7d835bcbefdadcec262
dsriggs1 <dsriggs1@gmail.com>
2024-11-18 22:59:34 -0500

    Adding additional steps to install script.

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p git
+
 curl https://raw.githubusercontent.com/dsriggs1/dotfiles/main/disko/btrfs-subvolumes.nix -o /tmp/btrfs-subvolumes.nix
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/btrfs-subvolumes.nix --arg device '"/dev/vda"'
 
 sudo nixos-generate-config --no-filesystems --root /mnt
-nix-shell -p git --run "git clone https://github.com/dsriggs1/dotfiles"
 cd /mnt/etc/nixos || exit 1
+#nix-shell -p git --run "git clone https://github.com/dsriggs1/dotfiles"
+sudo git clone https://github.com/dsriggs1/dotfiles
+sudo git checkout install
+sudo rm hardware-configuration.nix
+sudo cp /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/dotfiles
+sudo cp -r /mnt/etc/nixos /mnt/etc/persist
+sudo rm -r /etc/nixos/*
+
+
 
 #curl https://raw.githubusercontent.com/vimjoyer/impermanent-setup/main/final/disko.nix -o /tmp/disko.nix
 #sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/vda"'
