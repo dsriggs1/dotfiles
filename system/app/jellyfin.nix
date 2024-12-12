654f8147dc751c1491021b86a3cd115e19d72ec9
dsriggs1 <dsriggs1@gmail.com>
2024-12-11 20:37:48 -0500

    Removing most of the nixarr module configurations and replacing with my own configuration module for jellyfin and other arrs.

--- system/app/jellyfin.nix
+++ system/app/jellyfin.nix
@@ -1,5 +1,31 @@
 {...}: {
   services.jellyfin = {
     enable = true;
+    openFirewall = true;
   };
+
+  services.lidarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.readarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  #  services.bazarr = {
+  #   enable = true;
+  #  openFirewall = true;
+  # };
 }
