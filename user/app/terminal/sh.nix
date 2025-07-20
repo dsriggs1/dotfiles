26934c23ea8d3ad24d948dd6c9b1c64d693b9bdc
dsriggs1 <dsriggs1@gmail.com>
2025-07-20 18:37:51 -0400

    Removing nushell banner message and invoking pfetch.

--- user/app/terminal/sh.nix
+++ user/app/terminal/sh.nix
@@ -112,24 +112,28 @@ in {
   programs.nushell = {
     enable = true;
     #configFile.source = ./../config.nu;
-    environmentVariables = {env.EDITOR = "nvim";};
+    environmentVariables = {
+      env.EDITOR = "nvim";
+    };
     shellAliases = myAliases;
 
     #shellAliases = {
     # c = "clear";
     # confq = "$env.EDITOR ~/.config/qtile/config.py";
     #};
-    extraConfig = ''                        $env.config = { edit_mode: vi }
+    extraConfig = ''                           $env.config = { edit_mode: vi }
 
-                        def --env cd_func [path: string] {
-                            cd $path
-                  eza -al --icons
+      $env.config = { show_banner:false }
+                           def --env cd_func [path: string] {
+                               cd $path
+                     eza -al --icons
 
-                        }
-                        alias cd = cd_func
+                           }
+                           alias cd = cd_func
 
-      #      zoxide init nushell | save -f ~/.zoxide.nu
-            source ~/.zoxide.nu
+         #      zoxide init nushell | save -f ~/.zoxide.nu
+               source ~/.zoxide.nu
+        pfetch
     '';
 
     #extraConfig = ''
