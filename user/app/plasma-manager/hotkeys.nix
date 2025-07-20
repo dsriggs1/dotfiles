9bfa59673793cea77a01a01f49197f0dac29f35b
dsriggs1 <dsriggs1@gmail.com>
2025-07-20 18:24:36 -0400

    Adding hotkey for firefox.

--- user/app/plasma-manager/hotkeys.nix
+++ user/app/plasma-manager/hotkeys.nix
@@ -1,10 +1,18 @@
 {...}: {
   programs.plasma = {
     enable = true;
-    hotkeys.commands."launch-alacritty" = {
-      name = "Launch Alacritty";
-      key = "Meta+Return";
-      command = "alacritty";
+    hotkeys.commands = {
+      launch-alacritty = {
+        name = "Launch Alacritty";
+        key = "Meta+Return";
+        command = "alacritty";
+      };
+
+      launch-firefox = {
+        name = "Launch Firefox";
+        key = "Meta+Shift+B";
+        command = "firefox";
+      };
     };
   };
 }
