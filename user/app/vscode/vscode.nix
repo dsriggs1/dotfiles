41e6f6824e33d0872231e7553e1703eec0807207
dsriggs1 <dsriggs1@gmail.com>
2025-08-13 15:50:54 -0400

    Adding vscode configuration to make windows trusted by default.

--- user/app/vscode/vscode.nix
+++ user/app/vscode/vscode.nix
@@ -1,12 +1,18 @@
-{ config, pkgs, ...}:
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        github.copilot
-        ms-python.python
-        jnoortheen.nix-ide        
-      ];
-      
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      ms-python.python
+      jnoortheen.nix-ide
+    ];
+
+    userSettings = {
+      "security.workspace.trust.enabled" = false;
     };
+  };
 }
