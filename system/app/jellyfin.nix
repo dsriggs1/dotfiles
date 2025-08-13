eb16fb4201e2adb4a1af9fa1766c02cf1fe504ab
dsriggs1 <dsriggs1@gmail.com>
2025-08-13 17:20:20 -0400

    Adding directories for jellyfin media library.

--- system/app/jellyfin.nix
+++ system/app/jellyfin.nix
@@ -1,5 +1,13 @@
 {...}: {
   services.jellyfin = {
     enable = true;
+    openFirewall = true;
+    dataDir = "/srv/jellyfin";
+    logDir = "/srv/jellyfin/log";
   };
+  systemd.tmpfiles.rules = [
+    "d /srv/media/movies 0750 jellyfin jellyfin -"
+    "d /srv/media/shows 0750 jellyfin jellyfin -"
+    "d /srv/media/fights 0750 jellyfin jellyfin -"
+  ];
 }
