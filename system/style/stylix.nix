2e26bbab4905e4af79d0cc87df870536fbafba6e
dsriggs1 <dsriggs1@gmail.com>
2025-08-13 17:21:09 -0400

    Stylix was giving some error so stripping down to bare minimum.

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  stylix.enable = true;
   stylix.image = ./lake-sunrise.jpg;
   stylix.polarity = "dark";
   stylix.cursor.size = 8;
@@ -7,19 +8,20 @@
 
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Ice";
-
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-      name = "JetBrainsMono Nerd Font Mono";
-    };
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-  };
+  #  stylix.opacity.terminal = 0.7;
+  # stylix.opacity.applications = 0.7;
+  #stylix.fonts = {
+  #monospace = {
+  #package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+  #name = "JetBrainsMono Nerd Font Mono";
+  #};
+  #sansSerif = {
+  #package = pkgs.dejavu_fonts;
+  #name = "DejaVu Sans";
+  #};
+  #serif = {
+  #package = pkgs.dejavu_fonts;
+  #name = "DejaVu Serif";
+  #};
+  #};
 }
