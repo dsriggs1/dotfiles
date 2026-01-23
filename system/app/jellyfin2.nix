1d029e3b531508485b232d8253af4deab14b50fe
dsriggs1 <dsriggs1@gmail.com>
2026-01-23 16:51:42 -0500

    Adding new file with jellyfin and *arr configurations.

--- system/app/jellyfin2.nix
+++ system/app/jellyfin2.nix
@@ -1,14 +1,12 @@
-{ config, pkgs, lib, ... }:
-
-{
+{...}: {
   ########################################
   # Shared folders & permissions
   ########################################
   # A shared "media" group so all services can read/write the same folders.
-  users.groups.media = { };
+  users.groups.media = {};
 
   # Add your user so you can manage files too.
-  users.users.sean.extraGroups = [ "media" ];
+  users.users.sean.extraGroups = ["media"];
 
   # Add the service users to the media group (these users are created by the modules).
   users.groups.media.members = [
@@ -26,7 +24,8 @@
     "d /srv 0755 root root - -"
     "d /srv/media 0775 sean media - -"
     "d /srv/media/movies 0775 sean media - -"
-    "d /srv/media/tv 0775 sean media - -"
+    "d /srv/media/shows 0775 sean media - -"
+    "d /srv/media/fights 0775 sean media - -"
     "d /srv/downloads 0775 sean media - -"
     "d /srv/downloads/incomplete 0775 sean media - -"
     "d /srv/downloads/completed 0775 sean media - -"
@@ -44,10 +43,10 @@
     #  - /srv/media/tv
   };
 
-  services.prowlarr.enable = true;  # Web UI on 9696 by default
+  services.prowlarr.enable = true; # Web UI on 9696 by default
 
-  services.radarr.enable = true;    # Web UI on 7878 by default
-  services.sonarr.enable = true;    # Web UI on 8989 by default
+  services.radarr.enable = true; # Web UI on 7878 by default
+  services.sonarr.enable = true; # Web UI on 8989 by default
 
   services.jellyseerr = {
     enable = true;
@@ -57,26 +56,16 @@
     # port = 5055;
   };
 
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;   # opens the WebUI port
-    # The module exposes WebUI under 8080 by default; if you want to change it:
-    # webUI.port = 8080;
-    # You’ll set save paths in the qBittorrent UI to:
-    #  - Incomplete: /srv/downloads/incomplete
-    #  - Completed:  /srv/downloads/completed
-  };
-
   ########################################
   # Firewall (explicitly open the *arr* ports)
   ########################################
   networking.firewall.allowedTCPPorts = [
-    8096  # Jellyfin
-    9696  # Prowlarr
-    7878  # Radarr
-    8989  # Sonarr
-    5055  # Jellyseerr
-    8080  # qBittorrent WebUI
+    8096 # Jellyfin
+    9696 # Prowlarr
+    7878 # Radarr
+    8989 # Sonarr
+    5055 # Jellyseerr
+    8080 # qBittorrent WebUI
   ];
 
   # If you want to use qBittorrent’s default bittorrent ports:
