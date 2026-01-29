5e2744aa7241b0a093a045bb3e977375476fcf28
dsriggs1 <dsriggs1@gmail.com>
2026-01-28 19:17:54 -0500

    refactor(firefox): migrate bookmarks to new home-manager API

--- user/app/browser/firefox.nix
+++ user/app/browser/firefox.nix
@@ -11,67 +11,70 @@
         #         ublock-origin
         #      ];
         bookmarks = {
-          toolbar = {
-            name = "My Toolbar";
-            toolbar = true;
+          force = true;
+          settings = [
+            {
+              name = "My Toolbar";
+              toolbar = true;
 
-            bookmarks = [
-              {
-                name = "Nixos Wiki";
-                tags = ["nixos"];
-                keyword = "nixos";
-                url = "https://wiki.nixos.org/wiki/NixOS_Wiki";
-              }
-              {
-                name = "Jellyfin Server";
-                tags = ["Jellyfin"];
-                keyword = "Jellyfin";
-                url = "http://localhost:8096";
-              }
+              bookmarks = [
+                {
+                  name = "Nixos Wiki";
+                  tags = ["nixos"];
+                  keyword = "nixos";
+                  url = "https://wiki.nixos.org/wiki/NixOS_Wiki";
+                }
+                {
+                  name = "Jellyfin Server";
+                  tags = ["Jellyfin"];
+                  keyword = "Jellyfin";
+                  url = "http://localhost:8096";
+                }
 
-              {
-                name = "Bazarr";
-                tags = ["Bazarr"];
-                keyword = "Bazarr";
-                url = "http://localhost:6767";
-              }
+                {
+                  name = "Bazarr";
+                  tags = ["Bazarr"];
+                  keyword = "Bazarr";
+                  url = "http://localhost:6767";
+                }
 
-              {
-                name = "Lidarr";
-                tags = ["Lidarr"];
-                keyword = "Lidarr";
-                url = "http://localhost:8686";
-              }
+                {
+                  name = "Lidarr";
+                  tags = ["Lidarr"];
+                  keyword = "Lidarr";
+                  url = "http://localhost:8686";
+                }
 
-              {
-                name = "Prowlarr";
-                tags = ["Prowlarr"];
-                keyword = "Prowlarr";
-                url = "http://localhost:9696";
-              }
+                {
+                  name = "Prowlarr";
+                  tags = ["Prowlarr"];
+                  keyword = "Prowlarr";
+                  url = "http://localhost:9696";
+                }
 
-              {
-                name = "Radarr";
-                tags = ["Radarr"];
-                keyword = "Radarr";
-                url = "http://localhost:7878";
-              }
+                {
+                  name = "Radarr";
+                  tags = ["Radarr"];
+                  keyword = "Radarr";
+                  url = "http://localhost:7878";
+                }
 
-              {
-                name = "Sonarr";
-                tags = ["Sonarr"];
-                keyword = "Sonarr";
-                url = "http://localhost:8989";
-              }
+                {
+                  name = "Sonarr";
+                  tags = ["Sonarr"];
+                  keyword = "Sonarr";
+                  url = "http://localhost:8989";
+                }
 
-              {
-                name = "github";
-                tags = ["github"];
-                keyword = "github";
-                url = "https://github.com/";
-              }
-            ];
-          };
+                {
+                  name = "github";
+                  tags = ["github"];
+                  keyword = "github";
+                  url = "https://github.com/";
+                }
+              ];
+            }
+          ];
         };
       };
     };
