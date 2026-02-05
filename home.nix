e69428f36c03afa4aabcd3c1064957f98b59635e
dsriggs1 <dsriggs1@gmail.com>
2026-02-04 19:25:20 -0500

    config(home): wire stylix wallpaper/fonts symlinks and keybindings JSON, switch wallpaper

--- home.nix
+++ home.nix
@@ -1,7 +1,9 @@
 {
+  config,
   pkgs,
   lib,
   userSettings,
+  keybindings,
   pkgs-stable,
   ...
 }: {
@@ -24,8 +26,9 @@
 
   # Example themes: https://github.com/tinted-theming/schemes/tree/spec-0.11/base16
   # Some example color schemes that can be turned on/off
-  stylix.image = ./system/style/lake-sunrise.jpg;
+  stylix.image = ./wallpapers/Fantasy-Autumn.png;
   stylix.targets.gnome.enable = false;
+  programs.alacritty.enable = true;
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
   #  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
   #stylix.base16Scheme = "/home/sean/Downloads/dotfiles/themes/cyborg_girl";
@@ -42,6 +45,20 @@
     # GitHub SSH setup script
     (writeShellScriptBin "setup-github-ssh" (builtins.readFile ./scripts/setup-github-ssh.sh))
   ];
+
+  home.file.".config/stylix/wallpaper".source = config.stylix.image;
+
+  home.file.".config/stylix/fonts.json" = {
+    text = builtins.toJSON {
+      desktop = config.stylix.fonts.sizes.desktop;
+      applications = config.stylix.fonts.sizes.applications;
+      terminal = config.stylix.fonts.sizes.terminal;
+    };
+  };
+
+  home.file.".config/qtile/keybindings.json" = {
+    text = builtins.toJSON keybindings;
+  };
 
   home.file.".config/qtile/config.py" = {
     source = ./qtile/config.py;
