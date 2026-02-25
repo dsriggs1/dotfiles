ab5daee59d62cec06d67b2b4765bf8e8c482fa37
dsriggs1 <dsriggs1@gmail.com>
2026-02-25 14:22:33 -0500

    config(virt-manager): add VM variant for testing
    Add vmVariant configuration for building bootable VM images:
    - 20GB disk, 4GB RAM, 4 CPU cores
    - Host CPU passthrough for better performance
    - Test password for VM user access

--- system/app/virt-manager.nix
+++ system/app/virt-manager.nix
@@ -1,4 +1,15 @@
-{...}: {
+{lib, ...}: {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  virtualisation.vmVariant = {
+    users.users.sean = {
+      hashedPasswordFile = lib.mkForce null;
+      initialPassword = "test";
+    };
+    virtualisation.diskSize = 20480;
+    virtualisation.memorySize = 4096;
+    virtualisation.cores = 4;
+    virtualisation.qemu.options = ["-cpu" "host"];
+  };
 }
