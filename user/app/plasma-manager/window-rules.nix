0e426c861cc816138f0ca40127c1072ee336000c
dsriggs1 <dsriggs1@gmail.com>
2026-02-25 14:21:08 -0500

    config(plasma): add window rules for qalculate and Zen Browser
    Add KDE Plasma window rules:
    - qalculate: force floating, disable transparency, keep above
    - Zen Browser (zen-beta): assign to Desktop 2

--- user/app/plasma-manager/window-rules.nix
+++ user/app/plasma-manager/window-rules.nix
@@ -3,6 +3,31 @@
     enable = true;
     window-rules = [
       {
+        description = "Make qalculate float and disable transparency";
+        match = {
+          window-class = {
+            value = "qalculate";
+            type = "substring";
+            match-whole = false;
+          };
+        };
+        apply = {
+          opacityactive = {
+            value = 100;
+            apply = "force";
+          };
+          opacityinactive = {
+            value = 100;
+            apply = "force";
+          };
+          above = {
+            value = true;
+            apply = "force";
+          };
+          tilingrule = "2"; # Force floating (2 = force floating)
+        };
+      }
+      {
         description = "Disable transparency for Gwenview";
         match = {
           window-class = {
@@ -65,6 +90,20 @@
           desktopsrule = "3";
         }; # apply = "initially";
       } # };
+      {
+        description = "Assign Zen Browser to Desktop 2";
+        match = {
+          window-class = {
+            value = "zen-beta";
+            type = "substring";
+            match-whole = true;
+          };
+        };
+        apply = {
+          desktops = "Desktop_2";
+          desktopsrule = "3";
+        };
+      }
     ];
   };
 }
