0117c4ebf48333971d379f6d9335213c389ac55c
dsriggs1 <dsriggs1@gmail.com>
2026-02-27 22:53:54 -0500

    style(stylix): revert wallpaper to Fantasy-Autumn

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   stylix.enable = true;
-  stylix.image = ../../wallpapers/cyborg_girl_clean.png;
+  stylix.image = ../../wallpapers/Fantasy-Autumn.png;
   stylix.polarity = "dark";
   stylix.cursor.size = 24;
   stylix.fonts.sizes.applications = 12;
